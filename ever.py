a877c3390fec87341886d03bf55ba251803c948f
Ska <sergiosicari@gmail.com>
2014-12-05 07:03:54 +0100

    token string in a token file is better

--- ever.py
+++ ever.py
@@ -17,6 +17,7 @@ from evercode import EverCode
 from pygCode import PygCode
 
 class EvernoteCommand(sublime_plugin.TextCommand):
+
 	def run(self, edit):
 		extFixer = {'.py':'python'}
 		# select text
@@ -30,28 +31,51 @@ class EvernoteCommand(sublime_plugin.TextCommand):
 		for sel in sels:
 			selected_rows.append(self.view.substr(sel))
 
-		setting = Local();
-		if (setting.developer_token is None) or (setting.developer_token is ''):
-			sublime.error_message('set your developer token in settings!')
+		try:
+			with open("token") as file:
+				token_string = file.read()
+				file.close();
+				if (token_string is None) or (token_string is ''):
+					sublime.error_message("Your token string is Empty! Set it on token file in root directory plugin.");
+					return False;
+		except IOError as e:
+			sublime.error_message("Your token file dosn't not exist! You need a token file on root directory plugin.");
+			print "Unable to open file. Does not exist or no read permissions"
 			return False
-		evercode = EverCode(setting)
+
+		token = token_string.strip()
+
+		setting = Local();
+		setting.developer_token = token
+
+		self.evercode = EverCode(setting)
 
 		code = PygCode()
 		''' set language '''
 		code.setLexer(language)
 
 		''' set content '''
-		#source = code.getFileContent("evercode.py")
 		# set content from selection
 		source = ''.join(selected_rows)
-		body = code.getHighLightCode(source)
+		self.body = code.getHighLightCode(source)
 
 		''' set title '''
-		title = "test-da-sublime"
-		sublime.Window.showInputPanel('add a title to this snippet', '', None, None, None)
+		self.title = "snippet from sublime"
+		self.placeholder_title = "snippet from sublime"
+		self.view.window().show_input_panel("Snippet title", self.placeholder_title, self.set_title, None, None)
 
+	def set_title(self, title):
+		if not title:
+			title = self.title
 		''' connect to evercode account '''
-		evercode.setClient();
+		self.evercode.setClient();
 		''' send snippet '''
-		if evercode.makeNote(title, body) is not None:
-			sublime.status_message("snippet created on evernote account")
+		try:
+			if self.evercode.makeNote(title, self.body) is not None:
+				sublime.status_message("snippet created on evernote account")
+			else: 
+				sublime.status_message("snippet was empty")
+		except:
+			sublime.error_message('There was a problem on snippet creation!');
+			return False
+
